f67da57d9264abade312240392dcd70a9d53c931
shmily522 <57443191+shmily522@users.noreply.github.com>
2019-11-21 16:48:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -22,9 +22,9 @@ var config = {
     ],
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "氛围",
+        turn_on: "试着点一下？",
+        play: "来点音乐？",
+        bannar_coming: "缺点氛围呢",
         balloons_flying: "好像还少点东西呢！",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
